399f6c2b8c02a5c3a887ad517af15dc10c84ce51
Fiorella Aquino <fiorellaaquino05@gmail.com>
2023-02-16 21:36:57 -0300

    parte 1

---
+++ client/src/components/ProductForm.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import axios from 'axios';
+import { Field, Formik, Form } from 'formik';
+import * as Yup from 'yup';
+
+const ProductForm = () => {
+    const initialValues= {
+        title: '', 
+        price: '', 
+        description: ''
+    };
+
+    const productError= Yup.object().shape({
+        title: Yup.string()
+            .min(3, "Title must be at least 3 characters.")
+            .max(50, "Title should not be longer than 50 characters.")
+            .required("Title is required"),
+        price: Yup.number()
+            .integer("Price must be an integer")
+            .positive("Price should not be a negative number")
+            .required("Price is required"),
+        description: Yup.string()
+            .min(10, "Description must be at least 10 characters.")
+            .max(200, "Description should not be longer than 200 characters.")
+            .required("Description is required"),
+    });
+
+    const onSubmitHandler= async (values, actions) =>{
+        try {
+            const res= await axios.post("http://localhost:8000/api/product", values)
+            console.log(res);
+            actions.resetForm(initialValues);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <Formik
+            enableReinitialize={true}
+            initialValues={initialValues}
+            onSubmit={onSubmitHandler}
+            validationSchema={productError}
+        >
+        {({errors, touched, isValid, dirty}) => (
+            <Form>
+                <h1>Product Manager</h1>
+                <div className='d-flex p-2 bg-secondary bg-opacity-10 rounded mb-2'>
+                    <label htmlFor='title' className='m-2'>Title</label>
+                    <Field name="title" className="form-control h-10" />
+                </div>
+                {touched.title && errors.title && <p className="ms-5 text-danger">* {errors.title}</p>}
+                <div className='d-flex p-2 bg-secondary bg-opacity-10 rounded mb-2'>
+                    <label htmlFor='price' className='m-2'>Price</label>
+                    <Field name="price" type="number" className="form-control" />
+                </div>
+                {touched.price && errors.price && <p className="ms-5 text-danger">* {errors.price}</p>}
+                <div className='d-flex p-2 bg-secondary bg-opacity-10 rounded'>
+                    <label htmlFor='description' className='m-2'>Description</label>
+                    <Field name="description" className="form-control" />
+                </div>
+                {touched.description && errors.description && <p className="ms-5 text-danger">* {errors.description}</p>}
+
+                <button type='submit' className="btn btn-primary mt-3" disabled={!(isValid && dirty)}>Create</button>
+            </Form>
+        )}
+        </Formik>
+    )
+};
+
+export default ProductForm
---
+++ client/src/views/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductForm from '../components/ProductForm'
+
+const Main = () => {
+    return (
+        <div>
+            <div className='col-4 p-3'>
+                <ProductForm />
+            </div>
+        </div>
+    )
+};
+
+export default Main;
